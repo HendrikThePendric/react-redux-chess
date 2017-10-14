edbcd02f1263a8cc24f6cc15db994cd6f67e67e5
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2017-10-14 14:34:46 +0200

    Adds basic project structure and dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>FEN INPUT</h1>
+        <h1>MOVE INPUTS</h1>
+        <h1>BOARD</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
