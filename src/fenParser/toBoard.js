72471140cdf92e148d4ed7b91c2e53cc10e09852
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2017-10-14 20:51:45 +0200

    Removes test FEN string from FEN parser

--- src/fenParser/toBoard.js
+++ src/fenParser/toBoard.js
@@ -6,20 +6,19 @@ import {
 const addBoardItem = (board, rowNr, colIndex, instr) => {
   const colLetter = COLUMN_LETTERS[colIndex];
   board[rowNr + colLetter] = {
-    row: rowNr,
-    column: colLetter,
-    color: (rowNr % 2 === (colIndex + 1) % 2) ? 'white' : 'black',
+    row       : rowNr,
+    column    : colLetter,
+    color     : (rowNr % 2 === (colIndex + 1) % 2) ? 'white' : 'black',
     chessPiece: instr ? CHESS_PIECES[instr] : null
   };
 };
 
 const toBoard = fenStr => {
-  fenStr = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2';
-  let rowNr = 8;
-  let board = {};
+  let rowNr  = 8;
+  let board  = {};
   const rows = fenStr.split(' ')[0].split('/');
   rows.forEach(row => {
-    let colIndex = 0;
+    let colIndex          = 0;
     const colInstructions = row.split('');
     colInstructions.forEach(instr => {
       if (isNaN(parseInt(instr, 10))) {
@@ -37,7 +36,7 @@ const toBoard = fenStr => {
     rowNr--;
   });
   console.log(board);
-  return fenStr;
+  return board;
 };
 
 export default toBoard;
